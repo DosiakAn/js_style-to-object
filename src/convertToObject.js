4c7aff52f57b44d20437982f58abb08c5ec52f59
Anastasiia <anastasiia.dosiak@gmail.com>
2025-09-29 15:08:48 +0300

    Add solutions

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,22 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const newSourceString = sourceString.split('\n');
+  const newSourceString = sourceString.split(';');
   const resSourceString = [];
 
   for (let i = 0; i < newSourceString.length; i++) {
     const [key, value] = newSourceString[i].split(':');
 
-    resSourceString.push(key);
-    resSourceString.push(value);
+    if (key.trim() && value) {
+      resSourceString.push({ [key.trim()]: value.trim() });
+    }
   }
 
-  const styles = resSourceString.reduce((accum, currentValue, index, item) => {
-    if (index % 2 === 0) {
-      accum._key = currentValue;
-    } else {
-      accum[accum._key] = currentValue;
-    }
+  const styles = resSourceString.reduce((accum, currentValue) => {
+    const [key, value] = Object.entries(currentValue)[0];
+
+    accum[key] = value;
 
     return accum;
   }, {});
